4902e369958b7fb7d4b6c32fd2aa905990792f84
Jeff MAURY <jmaury@redhat.com>
2017-01-13 13:28:26 +0100

    [JBIDE-23724]

---
+++ itests/org.jboss.tools.ws.reddeer/src/org/jboss/tools/ws/reddeer/ui/wizards/jst/jsp/NewJSPFileWizard.java
@@ -0,0 +1,17 @@
+package org.jboss.tools.ws.reddeer.ui.wizards.jst.jsp;
+
+import org.jboss.reddeer.jface.wizard.NewWizardDialog;
+
+/**
+ * JSP File wizard.
+ *
+ * Web > JSP File
+ *
+ * @author Radoslav Rabara
+ *
+ */
+public class NewJSPFileWizard extends NewWizardDialog {
+	public NewJSPFileWizard() {
+		super("Web", "JSP File");
+	}
+}
